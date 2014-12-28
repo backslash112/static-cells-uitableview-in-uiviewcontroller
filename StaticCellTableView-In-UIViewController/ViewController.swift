2a32cfabd93d87e2950c747ed759b44982580dd0
Yang Cun <yangcun@live.com>
2014-12-28 14:06:40 +0800

    first commit.

---
+++ StaticCellTableView-In-UIViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StaticCellTableView-In-UIViewController
+//
+//  Created by YangCun on 14/12/28.
+//  Copyright (c) 2014年 backslash112. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
